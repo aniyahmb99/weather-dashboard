a0613869444d392978dc895c30aa30c4a1f472e2
aniyahmb99 <aniyah_mb@yahoo.com>
2022-03-19 00:05:49 -0400

    working on geocoding API

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+var currentTemp = document.getElementById("#temp");
+var currentWind = document.getElementById("#wind");
+var currentHumidity = document.getElementById("#humidity");
+var UVIndex = document.getElementById("#UVIndex");
+
+var btn = document.querySelector("#btn");
+
+// save user's city into a variable?
+var saveCity = function () {
+    event.preventDefault();
+    var city = document.getElementById("city").value;
+    localStorage.setItem("city", city);
+};
+
+var getCoordinates = function (coordinates) {
+
+    var geocodingUrl = "http://api.openweathermap.org/geo/1.0/direct?q=London,826&limit=1&appid=974fa9cd5b93c10b8e96c09931208dc7";
+
+    fetch(geocodingUrl).then(function (respone) {
+        // if request is successful
+        if (Response.ok) {
+            Response.json().then(function (data) {
+                secondFunction(data); // fix name later
+            }
+            )
+        }
+    })
+};
+btn.addEventListener("click", saveCity);
+
+// api key: 974fa9cd5b93c10b8e96c09931208dc7
+
+// var oneCallUrl = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+// // cityname to lat/long converter API
+// var geocodingUrl = http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
