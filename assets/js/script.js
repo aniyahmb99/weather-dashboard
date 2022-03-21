90f8b525910e10e669982cdd4a2f6c048f213c45
aniyahmb99 <aniyah_mb@yahoo.com>
2022-03-20 21:42:36 -0400

    finishing touches and overall redesign

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,99 @@
-var currentTemp = document.getElementById("#temp");
-var currentWind = document.getElementById("#wind");
-var currentHumidity = document.getElementById("#humidity");
-var UVIndex = document.getElementById("#UVIndex");
-var searchHistory = JSON.parse(localStorage.getItem("#city")); // ??
-var btn = document.querySelector("#btn");
+function myFunction() {
+    const cityEl = document.getElementById("userCity");
+    const searchEl = document.getElementById("searchBtn");
+    const cityNameEl = document.getElementById("cityName");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    var fiveDayEl = document.getElementById("fiveDayHeader");
+    var currentWeatherEl = document.getElementById("todayWeather");
 
-// save user's city into a variable? 
-var saveCity = function () {
-    event.preventDefault();
-    var city = document.getElementById("city").value;
-    localStorage.setItem("city", city);
-};
+    const APIKey = "974fa9cd5b93c10b8e96c09931208dc7";
 
-var getCoords = function (cityName) {
-    var lat = ;
-    var lon = ;
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=974fa9cd5b93c10b8e96c09931208dc7');
-        .then(response => response.json())
-    .then(data => console.log(data))
+    function getWeather(cityName) {
 
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+        axios.get(queryURL)
+            .then(function (response) {
+
+                currentWeatherEl.classList.remove("d-none");
+
+                const currentDate = new Date(response.data.dt * 1000);
+                const day = currentDate.getDate();
+                const month = currentDate.getMonth() + 1;
+                const year = currentDate.getFullYear();
+                cityNameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+
+                currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+                currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+                currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+
+                let lat = response.data.coord.lat;
+                let lon = response.data.coord.lon;
+                let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+                axios.get(UVQueryURL)
+                    .then(function (response) {
+                        let UVIndex = document.createElement("span");
+
+
+                        if (response.data[0].value < 4) {
+                            UVIndex.setAttribute("class", "badge badge-success");
+                        }
+                        else if (response.data[0].value < 8) {
+                            UVIndex.setAttribute("class", "badge badge-warning");
+                        }
+                        else {
+                            UVIndex.setAttribute("class", "badge badge-danger");
+                        }
+                        console.log(response.data[0].value)
+                        UVIndex.innerHTML = response.data[0].value;
+                        currentUVEl.innerHTML = "UV Index: ";
+                        currentUVEl.append(UVIndex);
+                    });
+
+
+                let cityID = response.data.id;
+                let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+                axios.get(forecastQueryURL)
+                    .then(function (response) {
+                        fiveDayEl.classList.remove("d-none");
+
+
+                        const forecastEls = document.querySelectorAll(".forecast");
+                        for (i = 0; i < forecastEls.length; i++) {
+                            forecastEls[i].innerHTML = "";
+                            const forecastIndex = i * 8 + 4;
+                            const forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
+                            const forecastDay = forecastDate.getDate();
+                            const forecastMonth = forecastDate.getMonth() + 1;
+                            const forecastYear = forecastDate.getFullYear();
+                            const forecastDateEl = document.createElement("p");
+                            forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
+                            forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+                            forecastEls[i].append(forecastDateEl);
+                            const forecastWeatherEl = document.createElement("img");
+                            forecastEls[i].append(forecastWeatherEl);
+                            const forecastTempEl = document.createElement("p");
+                            forecastTempEl.innerHTML = "Temp: " + k2f(response.data.list[forecastIndex].main.temp) + " F";
+                            forecastEls[i].append(forecastTempEl);
+                            const forecastHumidityEl = document.createElement("p");
+                            forecastHumidityEl.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
+                            forecastEls[i].append(forecastHumidityEl);
+                        }
+                    })
+            });
+    }
+
+    // Get history from local storage if any
+    searchEl.addEventListener("click", function () {
+        const searchTerm = cityEl.value;
+        getWeather(searchTerm);
+    })
+
+    function k2f(K) {
+        return Math.floor((K - 273.15) * 1.8 + 32);
+    }
 }
 
-getCoords();
-
-btn.addEventListener("click", saveCity);
-
-
-
+myFunction();
